4a90d6f302fc2095f3ed93d2fffb7997b1f4f952
temidore <temidorep@gmail.com>
2023-02-18 11:05:06 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 /**
- * main - prints all possible combinations of single digit numbers.
+ * main - print numbers from 00 to 99.
 (*
  * Return: 0 on success
  */
 int main(void)
 {
 	int i = '0';
+	int j = '0';
 
 	while (i <= '9')
 	{
-		putchar(i);
-		if (i != '9')
+		while (j <= '9')
 		{
-			putchar(',');
-			putchar(' ');
+			if (!(i > j || i == j))
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
 		}
-		++i;
+		j = '0';
+		i++;
 	}
-	putchar('\n');
 	return (0);
-
 }
-
