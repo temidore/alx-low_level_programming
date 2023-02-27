e39208448d2e6fcb633e2687e2f9b1cb8c21fa19
temidore <temidorep@gmail.com>
2023-02-27 13:08:20 -0800

    fixed

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c: The character to 0x05. C - Pointers, arrays and strings
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
