de7a6c1b4b170c7ed793ac8ad48d6c54fe966077
temidore <temidorep@gmail.com>
2023-03-13 08:41:41 -0700

    0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,18 +1,23 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
- * malloc_checked - Entry Point
- * @b: input amount
- * Return: pointer to new mem
+ * create_array - main entry
+ * @size: size input
+ * @c: char
+ * Return: 0
  */
-void *malloc_checked(unsigned int b)
+char *create_array(unsigned int size, char c)
 {
-	void *n;
+	char *array;
+	unsigned int i;
 
-	n = malloc(b);
+	array = malloc(sizeof(char) * size);
 
-	if (n == NULL)
-		exit(98);
-	return (n);
+	if (size == 0 || array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
 }
